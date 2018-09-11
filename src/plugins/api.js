c13ff7cbbd182ea02ee00ff7ef1e2aec7aeeedf9
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-09-11 16:17:19 +0800

    fix

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -38,5 +38,7 @@ export default {
   /** 资讯列表 */
   apiNewslist: host + '/news/list',
   /** 资讯详情 */
-  apiNewsDetail: host + '/news'
+  apiNewsDetail: host + '/news',
+  // 旅游订单列表
+  apiTravelOrderList: host + '/travelOrder/list'
 }
